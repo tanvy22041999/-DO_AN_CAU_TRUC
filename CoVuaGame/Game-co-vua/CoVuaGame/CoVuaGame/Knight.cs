febdaa3a57b86bb12fab0c0bd95cfced8d9c87fa
tanvy22041999 <55608297+tanvy22041999@users.noreply.github.com>
2019-11-29 01:47:50 +0700

    updata_Chess

---
+++ CoVuaGame/Game-co-vua/CoVuaGame/CoVuaGame/Knight.cs
@@ -0,0 +1,32 @@
+﻿
+ using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;                                //  
+                                                          //      
+namespace CoVuaGame
+    {
+        class Knight
+        {
+            public void CanMove(List<List<Button>> Matrix, Point point)
+            {
+                int[] A = { 2, 2, 1, 1, -1, -1, -2, -2 };
+                int[] B = { -1, 1, -2, 2, -2, 2, -1, 1 };
+                for (int i = 0; i < 8; i++)
+                {
+                    if (point.X + A[i] >= 0 && point.X + A[i] <= 7 && point.Y + B[i] >= 0 && point.Y + B[i] <= 7)
+                    {
+                        if (Matrix[point.X + A[i]][point.Y + B[i]].Name == "NULL")
+                        {
+                            Matrix[point.X + A[i]][point.Y + B[i]].BackColor = Color.LightBlue;
+                            Matrix[point.X + A[i]][point.Y + B[i]].Name = "A";
+                        }
+                    }
+                }
+            }
+        }
+    }
+
